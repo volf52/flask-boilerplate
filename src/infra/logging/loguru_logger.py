0d52021c4927651ee16e8469d9c03a89b1d7c4dd
Arslan <rslnkrmt2552@gmail.com>
2022-09-01 18:10:07 +0500

    remove unnecessary doc strings

--- src/infra/logging/loguru_logger.py
+++ src/infra/logging/loguru_logger.py
@@ -12,11 +12,6 @@ from loguru._defaults import LOGURU_FORMAT
 
 
 class InterceptHandler(logging.Handler):
-    """
-    Default handler from examples in loguru documentaion.
-    See https://loguru.readthedocs.io/en/stable/overview.html#entirely-compatible-with-standard-logging
-    """
-
     def emit(self, record: logging.LogRecord) -> None:
         # Get corresponding Loguru level if it exists
         try:
@@ -36,18 +31,6 @@ class InterceptHandler(logging.Handler):
 
 
 def format_record(record: dict[Any, Any]) -> str:
-    """
-    Custom format for loguru loggers.
-    Uses pformat for log any data like request/response body during debug.
-    Works with logging if loguru handler it.
-    Example:
-    >>> payload = [{"users":[{"name": "Nick", "age": 87, "is_active": True}, {"name": "Alex", "age": 27, "is_active": True}], "count": 2}]
-    >>> logger.bind(payload=).debug("users payload")
-    >>> [   {   'count': 2,
-    >>>         'users': [   {'age': 87, 'is_active': True, 'name': 'Nick'},
-    >>>                      {'age': 27, 'is_active': True, 'name': 'Alex'}]}]
-    """
-
     format_string = LOGURU_FORMAT
     if record["extra"].get("payload") is not None:
         record["extra"]["payload"] = pformat(
@@ -60,22 +43,6 @@ def format_record(record: dict[Any, Any]) -> str:
 
 
 def init_logging():
-    """
-    Replaces logging handlers with a handler for using the custom handler.
-
-    WARNING!
-    if you call the init_logging in startup event function,
-    then the first logs before the application start will be in the old format
-    >>> app.add_event_handler("startup", init_logging)
-    stdout:
-    INFO:     Uvicorn running on http://127.0.0.1:8000 (Press CTRL+C to quit)
-    INFO:     Started reloader process [11528] using statreload
-    INFO:     Started server process [6036]
-    INFO:     Waiting for application startup.
-    2020-07-25 02:19:21.357 | INFO     | uvicorn.lifespan.on:startup:34 - Application startup complete.
-
-    """
-
     # disable handlers for specific uvicorn loggers
     # to redirect their output to the default uvicorn logger
     # works with uvicorn==0.11.6
